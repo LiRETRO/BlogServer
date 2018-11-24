97f6c346e473a975dabe549e45890b6ebb8f6937
LiRETRO <hash@meloli.net>
2018-11-24 19:35:15 +0800

    按日期取出数据

--- src/main/java/net/meloli/demo/sys/service/impl/BlogServiceImpl.java
+++ src/main/java/net/meloli/demo/sys/service/impl/BlogServiceImpl.java
@@ -5,7 +5,9 @@ import net.meloli.demo.sys.mongodb.util.MongoDBUtils;
 import net.meloli.demo.sys.service.inf.IBlogService;
 import net.meloli.demo.sys.util.MvcDataDto;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.data.domain.Sort;
 import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -26,7 +28,9 @@ public class BlogServiceImpl implements IBlogService {
     @Override
     public MvcDataDto getBlogList() {
         MvcDataDto data = MvcDataDto.getInstance();
-        List<Blog> blogList =  mongoTemplate.findAll(Blog.class, MongoDBUtils.CollectionName.BLOG);
+        Query query = new Query();
+        query.with(new Sort(Sort.Direction.DESC, "blogPublishDate"));
+        List<Blog> blogList = mongoTemplate.find(query, Blog.class, MongoDBUtils.CollectionName.BLOG);
         data.setResultObj(blogList);
         data.setResultCode(MvcDataDto.SUCCESS);
         return data;
