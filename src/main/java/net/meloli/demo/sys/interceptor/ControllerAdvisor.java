a872c7135e2366b2352483402d4541c99e2b9a5a
LiRETRO <hash@meloli.net>
2018-12-19 14:17:02 +0800

    添加Controller下的切入点

---
+++ src/main/java/net/meloli/demo/sys/interceptor/ControllerAdvisor.java
@@ -0,0 +1,29 @@
+package net.meloli.demo.sys.interceptor;
+
+import org.aspectj.lang.ProceedingJoinPoint;
+import org.aspectj.lang.annotation.AfterReturning;
+import org.aspectj.lang.annotation.Around;
+import org.aspectj.lang.annotation.Aspect;
+import org.aspectj.lang.annotation.Pointcut;
+import org.springframework.stereotype.Component;
+
+@Component
+@Aspect
+public class ControllerAdvisor {
+
+    ThreadLocal<Long> timeHolder = new ThreadLocal<>();
+
+    /**
+     * 切入点
+     * 切入Controller包下所有类的所有方法
+     */
+    @Pointcut("execution(* *..*controller*..*(..))")
+    public void pointCut() {}
+
+    @Around("pointCut()")
+    public void around(ProceedingJoinPoint joinPoint) {
+
+    }
+
+
+}
